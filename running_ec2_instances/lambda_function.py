3ab822559d869f2cbdfa8d5a5ccc92364794782b
Stacey <124016788+StaceyM0@users.noreply.github.com>
2023-04-19 18:15:01 -0600

    Update lambda_function.py

--- running_ec2_instances/lambda_function.py
+++ running_ec2_instances/lambda_function.py
@@ -1,6 +1,13 @@
 import json
 import boto3
 
+import json
+import boto3
+import json
+import boto3
+import json
+import boto3
+
 def lambda_handler(event, context):
     
     ec2 = boto3.client('ec2')
